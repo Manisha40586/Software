41131e3fb58405759da1da56e3c8a8a8c68a07f5
Manisha40586 <lamichhanemanisha03@gmail.com>
2026-02-09 13:15:46 +0545

    Initial commit

---
+++ practise/index.js
@@ -0,0 +1,25 @@
+
+// console.log("unwanted buggy code")
+console.log("unwanted buggy code")
+
+let color="red"
+console.log("color",color);
+
+let fullName="manisha";
+console.log(fullName);
+
+ fullName ="Ram kumar"
+fullName = "Hari chettri"
+fullName = "Ram bhadur"
+console.log(fullName);
+
+let firstName = "Hari"
+let middlename= "kumar"
+let fullname = firstName + " " + middlename
+
+console.log(fullname)
+
+const PI = 3.1415743239053489;
+const PI = 3.14;
+
+console.log(PI);
